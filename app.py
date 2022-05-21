f9d2414cc95c1c25f9b279bb736abc1dded095bc
twopetersens <102703033+twopetersens@users.noreply.github.com>
2022-05-21 16:08:33 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,141 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 13,
+   "id": "ec1eb2ab",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import datetime as dt\n",
+    "import numpy as np\n",
+    "import pandas as pd"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 14,
+   "id": "b99d9d80",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import sqlalchemy\n",
+    "from sqlalchemy.ext.automap import automap_base\n",
+    "from sqlalchemy.orm import Session\n",
+    "from sqlalchemy import create_engine, func"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 15,
+   "id": "49f3550f",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from flask import Flask, jsonify"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 16,
+   "id": "47467c5a",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "engine = create_engine(\"sqlite:///hawaii.sqlite\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 17,
+   "id": "b360523b",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "Base = automap_base()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 18,
+   "id": "547ac25d",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "Base.prepare(engine, reflect=True)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 19,
+   "id": "90836fd2",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "Measurement = Base.classes.measurement\n",
+    "Station = Base.classes.station"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 20,
+   "id": "508311d9",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "session = Session(engine)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 22,
+   "id": "792b86a0",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "app = Flask(__name__)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 12,
+   "id": "00067c54",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "@app.route(\"/\")\n",
+    "def welcome():\n",
+    "    return(\n",
+    "    '''\n",
+    "    Welcome to the Climate Analysis API!\n",
+    "    Available Routes:\n",
+    "    /api/v1.0/precipitation\n",
+    "    /api/v1.0/stations\n",
+    "    /api/v1.0/tobs\n",
+    "    /api/v1.0/temp/start/end\n",
+    "    ''')"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
